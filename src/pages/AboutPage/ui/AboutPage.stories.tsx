26dbede3627c34c4e07d8bec968a41d2715de548
akolesnikov <akolesnikov@team.amocrm.com>
2023-10-15 18:54:54 +0300

    add stories for pages

---
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ThemeDecorator from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import AboutPage from './AboutPage';
+
+const meta = {
+    title: 'pages/AboutPage',
+    component: AboutPage,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof AboutPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+    args: {},
+};
+
+export const Dark: Story = {
+    args: {},
+    decorators: [
+        (Story) => (
+            <ThemeDecorator theme={Theme.DARK}>
+                <Story />
+            </ThemeDecorator>
+        ),
+    ]
+};
+
---
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ThemeDecorator from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import MainPage from './MainPage';
+
+const meta = {
+    title: 'pages/MainPage',
+    component: MainPage,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof MainPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+    args: {},
+};
+
+export const Dark: Story = {
+    args: {},
+    decorators: [
+        (Story) => (
+            <ThemeDecorator theme={Theme.DARK}>
+                <Story />
+            </ThemeDecorator>
+        ),
+    ]
+};
+
