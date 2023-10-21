2a307306d3275b3116cd7afa9d5d641882453557
akolesnikov <akolesnikov@team.amocrm.com>
2023-10-21 19:04:09 +0300

    update screenshots, add unit test for react router dom, and add report for loki

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { MemoryRouter } from 'react-router-dom';
+
+export interface СomponentRenderOptions {
+    route?: string;
+}
+
+export default
+function componentRender(component: ReactNode, options: СomponentRenderOptions = {}) {
+    const {
+        route = '/',
+    } = options;
+
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+            ,
+        </MemoryRouter>,
+    );
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
 
     return (
-        <div className={classNames(cls.Navbar, {}, [className])}>
+        <div data-testid="navbar" className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
                     {t('Main page')}
