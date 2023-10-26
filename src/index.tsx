825ba3e4d26a4d46f1ccf6cc7bbf7f1bfc95fe50
akolesnikov <akolesnikov@team.amocrm.com>
2023-10-26 09:11:22 +0300

    set theme during the mount

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import {
+    LOCAL_STORAGE_THEME_KEY, Theme,
+} from 'app/providers/ThemeProvider/lib/ThemeContext';
 import 'app/styles/index.scss';
 import App from './app/App';
 
 import 'shared/config/i18n/i18n';
 
 const root = createRoot(document.getElementById('root'));
+
+const theme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || Theme.LIGHT;
+document.body.className = theme;
 
 root.render(
     <StoreProvider>
