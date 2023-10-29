fe52cfe2e9d9458aff7c553d7959d1a2bca2545a
akolesnikov <akolesnikov@team.amocrm.com>
2023-10-29 03:14:43 +0300

    fix unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -108,6 +108,8 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        // Надо бы поисследовать, react-redux похоже в нём проблема
+        'entities/(.*)': '<rootDir>/src/entities/$1',
     },
 
     // An array of regexp pattern strings, matched against all module paths before considered
